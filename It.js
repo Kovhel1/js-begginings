86df7ab223c2e62785cd6d7fa5df4585cc259099
Elena Kovalieva <er_kovalieva@mail.ru>
2021-09-29 22:58:43 +0300

    keep going

---
+++ It.js
@@ -0,0 +1,6 @@
+let name = prompt("Scrivete il tuo nome", 'Helen');
+//alert(`Hello, ${name} !`);
+let age = prompt("Quanti anni hai?", 100);
+alert(typeof(age));
+age = Number(age);
+alert(typeof(age));
